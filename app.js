443717447e188d60e3d313f1bb8940dae60c7e3e
Kelechi Igbokwe <kelechi_igbokwe@yahoo.com>
2018-10-17 10:07:55 +0200

    added axios api

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const vm = new Vue({
     el: '#app',
     data: {
-      results: [
-        {title: "the very first post", abstract: "lorem ipsum some test dimpsum"},
-        {title: "and then there was the second", abstract: "lorem ipsum some test dimsum"},
-        {title: "third time's a charm", abstract: "lorem ipsum some test dimsum"},
-        {title: "four the last time", abstract: "lorem ipsum some test dimsum"}
-      ]
+      results: []
+    },
+    mounted() {
+      axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=2efcc4b0db5b4859979baa0f40156792")
+      .then(response => {this.results = response.data.results})
     }
   });
